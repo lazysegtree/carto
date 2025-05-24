3e342b02c92706048d8eb00b33a01291b2dcad72
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-05-24 23:06:58 +0800

    convert custom themes to use lists

--- src/themes.py
+++ src/themes.py
@@ -10,22 +10,21 @@ def get_custom_themes():
         dict: A dictionary of custom themes.
     """
     custom_themes = []
-    for key in state.config["interface"]["theme"].keys():
-        if type(state.config["interface"]["theme"][key]) is dict:
-            custom_themes.append(
-                Theme(
-                    name=state.config["interface"]["theme"][key]["name"],
-                    primary=state.config["interface"]["theme"][key]["primary"],
-                    secondary=state.config["interface"]["theme"][key]["secondary"],
-                    accent=state.config["interface"]["theme"][key]["accent"],
-                    foreground=state.config["interface"]["theme"][key]["foreground"],
-                    background=state.config["interface"]["theme"][key]["background"],
-                    success=state.config["interface"]["theme"][key]["success"],
-                    warning=state.config["interface"]["theme"][key]["warning"],
-                    error=state.config["interface"]["theme"][key]["error"],
-                    surface=state.config["interface"]["theme"][key]["surface"],
-                    panel=state.config["interface"]["theme"][key]["panel"],
-                    dark=state.config["interface"]["theme"][key]["is_dark"],
-                )
+    for theme in state.config["interface"]["custom_theme"]:
+        custom_themes.append(
+            Theme(
+                name=theme["name"].lower().replace(" ", "-"), # keep it similar to default textual behaviour
+                primary=theme["primary"],
+                secondary=theme["secondary"],
+                accent=theme["accent"],
+                foreground=theme["foreground"],
+                background=theme["background"],
+                success=theme["success"],
+                warning=theme["warning"],
+                error=theme["error"],
+                surface=theme["surface"],
+                panel=theme["panel"],
+                dark=theme["is_dark"],
             )
+        )
     return custom_themes
