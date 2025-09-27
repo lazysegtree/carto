71be207e5e6d693e6ad040e456b67e60398e1587
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-09-27 14:08:10 +0800

    remive zen mode because merge didnt work properly

--- src/rovr/app.py
+++ src/rovr/app.py
@@ -333,15 +333,6 @@ class Application(App, inherit_bindings=False):
                         )
 
                 self.push_screen(ZDToDirectory(), on_response)
-            # zen mode
-            case key if (
-                config["plugins"]["zen_mode"]["enabled"]
-                and key in config["plugins"]["zen_mode"]["keybinds"]
-            ):
-                if "zen" in self.classes:
-                    self.remove_class("zen")
-                else:
-                    self.add_class("zen")
             # keybinds
             case key if key in config["keybinds"]["show_keybinds"]:
                 self.push_screen(Keybinds())
